529d93abc4c68276b13e1673527945056ca489af
Ben Braun <benjamin.braun2003@gmail.com>
2026-06-12 18:17:30 -0500

    Add section transitions for mobile

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,19 @@ import './App.css'
 import Header from './Components/Header.tsx'
 import TodoSection from './Components/TodoSection'
 import MobileNavBar from './Components/MobileNavBar.tsx'
+import { useState } from 'react'
 
 
 function App() {
+
+	const [activeTab, setActiveTab] = useState("home")
+
+	// Helper function to translate the activeTab string into a CSS percentage shift
+	const getTranslateX = () => {
+		if (activeTab === "home") return "translate-x-0";       // Show page 1
+		if (activeTab === "todos") return "-translate-x-1/2";   // Slide left to show page 2
+		return "translate-x-0";
+	};
 
 	return (
 		<>
@@ -12,10 +22,23 @@ function App() {
 
 				<main className='w-full px-4 pt-4 pb-24'>
 					<Header />
-					<TodoSection />
+					<div className='w-full overflow-hidden'>
+
+						{/* 200% means it can hold 2 sections worth*/}
+						<div className={`flex w-[200%] transition-transform duration-300 ease-in-out ${getTranslateX()}`}>
+
+							{/* w-1/2 tells it how much to take up*/}
+							<section className='w-1/2 shrink-0'>
+								<TodoSection />
+							</section>
+							<section className='w-1/2 shrink-0'>
+								<TodoSection />
+							</section>
+						</div>
+					</div>
 				</main>
 
-				<MobileNavBar />
+				<MobileNavBar activeTab={activeTab} setActiveTab={setActiveTab} />
 
 
 			</div >
--- src/Components/MobileNavBar.tsx
+++ src/Components/MobileNavBar.tsx
@@ -1,7 +1,13 @@
-import { useState } from "react";
-function MobileNavBar() {
 
-	const [activeTab, setActiveTab] = useState("home");
+
+interface MobileNavBarProps {
+	activeTab: string;
+	setActiveTab: (tabId: string) => void;
+}
+
+
+function MobileNavBar({ activeTab, setActiveTab }: MobileNavBarProps) {
+
 
 	const navigationItems = [
 		{
@@ -15,21 +21,6 @@ function MobileNavBar() {
 			id: "todos", label: "Dashboard", icon: (
 				<svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" strokeWidth="2">
 					<path strokeLinecap="round" strokeLinejoin="round" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
-				</svg>
-			)
-		},
-		{
-			id: "projects", label: "Projects", icon: (
-				<svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" strokeWidth="2">
-					<path strokeLinecap="round" strokeLinejoin="round" d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4" />
-				</svg>
-			)
-		},
-
-		{
-			id: "Chess", label: "Chess", icon: (
-				<svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" strokeWidth="2">
-					<path strokeLinecap="round" strokeLinejoin="round" d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4" />
 				</svg>
 			)
 		},
