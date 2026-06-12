ad5f45a4fb9c9b92604a13b5df851035305551e9
Ben Braun <benjamin.braun2003@gmail.com>
2026-06-12 15:38:06 -0500

    Add Todo item and shadow on the todo card

---
+++ src/Components/Todo.tsx
@@ -0,0 +1,19 @@
+
+
+function Todo() {
+
+	return (
+
+
+		<div className=" border border-slate-400 bg-slate-700 rounded-lg p-2 w-full">
+			<h3>Do a Leetcode</h3>
+
+		</div>
+
+
+
+
+	)
+}
+
+export default Todo
--- src/Components/TodoCard.tsx
+++ src/Components/TodoCard.tsx
@@ -1,18 +1,22 @@
+import Todo from "./Todo"
 
 function TodoCard() {
 
 	return (
 
 
-		<div className="bg-slate-700 rounded-2xl w-11/12">
-			<header>
-				<div className="flex flex-col p-4">
+		<div className="flex flex-col bg-slate-700 rounded-2xl w-11/12 p-4 border border-slate-600/40 shadow-lg">
+			<header className="flex flex-col pb-5">
 
-					<h2 className="text-slate-100 font-sans font-bold text-xl pb-1">Dailies</h2>
-					<p className="text-slate-300">Refreshes Every Day</p>
-				</div>
+				<h2 className="text-slate-100 font-sans font-bold text-xl pb-1">Dailies</h2>
+				<p className="text-slate-300">Refreshes Every Day</p>
 
 			</header>
+
+			<div className="flex flex-col items-center">
+
+				<Todo />
+			</div>
 
 
 
