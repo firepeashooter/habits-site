59092d190050bb89e182fdecb2e0845c4f991ab7
Ben Braun <benjamin.braun2003@gmail.com>
2026-09-09 16:18:38 -0500

    backend: add sending username back for displaying when loging in

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -22,9 +22,11 @@ def register_user(request):
 
     serializer = RegisterSerializer(data=request.data)
 
+    username = request.data.get("username")
+
     if serializer.is_valid():
         serializer.save()
-        return Response({"message": "User created successfully!"}, status=status.HTTP_201_CREATED)
+        return Response({"message": "User created successfully!", "username": username}, status=status.HTTP_201_CREATED)
 
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
